bf15b3f80f2f47fc3e5cfbfe35c3ce6b3dd5b561
gaurav parajuli <gparajuli.1997@gmail.com>
2023-02-16 13:15:20 +0545

    draw is detected

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Square from './Square';
 
@@ -8,24 +8,76 @@ const Board = () => {
     const initialState = Array(9).fill(null);
 
     const [board, setBoard] = useState(initialState);
-    const [isXNext, setIsXNext] = useState(false);
+    const [isXTurn, setIsXTurn] = useState(true);
+    const [winner, setWinner] = useState(null);
+    const [noMovesLeft, setNoMovesLeft] = useState(false);
 
     const handleSquareClick = position => {
+        if (winner) return; // if game is over, donot allow users to click
         setBoard(state =>
             state.map((item, index) =>
-                position === index && !item ? (isXNext ? 'O' : 'X') : item
+                position === index && !item ? (isXTurn ? 'X' : 'O') : item
             )
         );
-        setIsXNext(state => !state);
+        if (!board[position]) setIsXTurn(state => !state);
     };
 
+    const checkWinner = () => {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ];
+
+        for (let i = 0; i < 8; i++) {
+            const [x, y, z] = lines[i];
+            if (board[x] && board[x] === board[y] && board[y] === board[z])
+                return board[x];
+        }
+    };
+
+    // check for winner
+    useEffect(() => {
+        const result = checkWinner();
+        if (result) setWinner(result);
+    }, [board]);
+
+    // check if now move left
+    useEffect(() => {
+        setNoMovesLeft(!board.includes(null) && winner === null);
+    }, [board]);
+
     return (
-        <div className="grid grid-cols-3 border-2">
+        <div className="grid grid-cols-3 text-white">
+            <div className="col-span-3 text-3xl text-center py-1">
+                {winner
+                    ? `${winner} wins the game!`
+                    : noMovesLeft
+                    ? 'Game over'
+                    : isXTurn
+                    ? 'X turn now!'
+                    : 'O turn now!'}
+            </div>
             {index.map(item => (
                 <Square onClick={() => handleSquareClick(item)} key={item}>
                     {board[item]}
                 </Square>
             ))}
+            <button
+                className="col-span-3 p-2 hover:underline border mt-2 rounded-lg"
+                onClick={() => {
+                    setBoard(initialState);
+                    setWinner(null);
+                    setIsXTurn(true);
+                }}
+            >
+                RESET GAME
+            </button>
         </div>
     );
 };
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Square = ({ children, onClick }) => {
     return (
         <button
-            className="border p-4 text-4xl max-h-[100px] max-w-[100px] min-h-[100px] min-w-[100px] text-white"
+            className="border p-4 text-4xl max-h-[100px] max-w-[100px] min-h-[100px] min-w-[100px] "
             onClick={onClick}
         >
             {children}
