d89f2a9d4ba3a5b882236ffe209e4023ffb4b9c4
gaurav parajuli <gparajuli.1997@gmail.com>
2023-02-16 06:19:50 +0545

    added board state logic

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Square from './Square';
 
 const Board = () => {
     const index = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
+    const [board, setBoard] = useState(Array(9).fill(null));
+
+    const handleSquareClick = position => {
+        setBoard(state =>
+            state.map((item, index) => (index === position ? 'X' : item))
+        );
+    };
+
     return (
-        <div className="grid grid-cols-3">
+        <div className="grid grid-cols-3 border-2">
             {index.map(item => (
-                <Square key={item}>{item}</Square>
+                <Square onClick={() => handleSquareClick(item)} key={item}>
+                    {board[item]}
+                </Square>
             ))}
         </div>
     );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
-const Square = ({ children }) => {
-    return <div>{children}</div>;
+const Square = ({ children, onClick }) => {
+    return (
+        <button
+            className="border p-4 text-4xl max-h-[100px] max-w-[100px] min-h-[100px] min-w-[100px]"
+            onClick={onClick}
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Square;
